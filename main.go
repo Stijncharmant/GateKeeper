adb61ff0d413e4a500360b8792d390c2390191ee
Stijn Charmant <s.charmant@student.fontys.nl>
2023-02-09 14:48:44 +0100

    versie 1.3 tijd toegevoegd en comments verwerkt

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt" //importeerd het pakket fmt waarmee je de println command kan gebruiken
+import (
+	"fmt"  //standard package for using println
+	"time" //standard package for using time
+)
 
 func main() {
-	fmt.Println("Hello, World!") //print hello world in de terminal
+	currentTime := time.Now().Hour() //gets the current time in hours
+
+	if currentTime >= 7 && currentTime <= 12 { //check the current time and if between set arguments prints the underlying line
+		fmt.Println("Goedemorgen! Welkom bij Fonteyn vakantieparken")
+	} else if currentTime >= 12 && currentTime <= 18 { //check the current time and if between set arguments prints the underlying line
+		fmt.Println("Goedemiddag! Welkom bij Fonteyn vakantieparken")
+	} else if currentTime >= 18 && currentTime <= 23 { //check the current time and if between set arguments prints the underlying line
+		fmt.Println("Goedenavond! Welkom bij Fonteyn vakantieparken")
+	}
 }
